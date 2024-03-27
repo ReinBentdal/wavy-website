c9b95676d69873f35bedf3634aa1bd9186301d34
ReinBentdal <rein.bent@gmail.com>
2024-03-27 13:50:36 +0100

    astro webpage init

--- src/routes/about/+page.svelte
+++
@@ -1,7 +0,0 @@
-<p>
-    We are a small team based in beautiful Norway, Lillehammer. We look into how new technologies might open up new doors for music making experiences.
-</p>
-
-<p>
-    Contact: Rein Gundersen Bentdal - rein.bent put the thing here gmail.com
-</p>
--- src/routes/midi-connect/+page.svelte
+++
@@ -1,3 +0,0 @@
-<p>Simple connection for any Bluetooth MIDI device.</p>
-<p>Coming soon..</p>
-<img width="400px" src="assets/midi connect.png" alt="MIDI Connect for macOS" />
