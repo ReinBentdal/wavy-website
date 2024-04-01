ce94df77096969fdd75370e201a38033f3373ca3
ReinBentdal <rein.bent@gmail.com>
2024-04-01 02:35:08 +0200

    overall improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     build: {
         format: 'file',
-    }
+    },
+    vite: {
+        resolve: {
+          alias: {
+            // Sets up an alias so you can import from "src/" as if it were a root
+            '~/': new URL('./src/', import.meta.url).pathname,
+          },
+        },
+      },
 });
