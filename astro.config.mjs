3d2bdefe7071609c70b4e8e0dffbcfd33d4905ab
ReinBentdal <rein.bent@gmail.com>
2024-04-10 17:52:05 +0200

    documentation update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        format: 'file',
-        assets: 'build_assets'
-    },
-    vite: {
-        resolve: {
-          alias: {
-            // Sets up an alias so you can import from "src/" as if it were a root
-            '~/': new URL('./src/', import.meta.url).pathname,
-          },
-        },
-      },
+  build: {
+    format: 'file',
+    assets: 'build_assets'
+  },
+  vite: {
+    resolve: {
+      alias: {
+        // Sets up an alias so you can import from "src/" as if it were a root
+        '~/': new URL('./src/', import.meta.url).pathname
+      }
+    }
+  },
+  integrations: [playformCompress()]
 });
