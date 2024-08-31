205e702eb6b40d886d4bbacc85d2903af57af43f
ReinBentdal <rein.bent@gmail.com>
2024-08-31 02:07:56 +0200

    new design init

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+// make it easier to change routes by defining them in one place
+export const routes = {
+    'home': './',
+    'monkey': './monkey',
+    'KD-3': './KD-3',
+    'monkey-manual': './resources/guides/monkey-manual',
+    'monkey-getting-started': './monkey/getting-started',
+    'device-update': './resources/device-update',
+}
