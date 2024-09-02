e3fbe2daa5e04064c2f1856e0f13ba8fdef268a5
ReinBentdal <rein.bent@gmail.com>
2024-09-02 16:25:47 +0200

    monkey page with animations, manual init

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,7 @@ export const routes = {
     'monkey-manual': './resources/guides/monkey-manual',
     'monkey-getting-started': './monkey/getting-started',
     'device-update': './resources/device-update',
+    'MIDI-connect': './MIDI-connect',
+    'wavy-discord': 'https://discord.gg/qm9ggkDzdc',
+    'midi-connect-app-store': 'https://apps.apple.com/no/app/midi-toolbar/id6476070288?mt=12'
 }
