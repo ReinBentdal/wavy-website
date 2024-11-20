0e66e4d2b9dc1c429527444065e5adfe6075164d
ReinBentdal <rein.bent@gmail.com>
2024-11-20 16:44:52 +0100

    Updates

--- build_assets/hoisted.BVd_L1S7.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.5jgjDlby.js";function l(){let e=document.getElementById("something-special-kd3"),t="hell",i="o";e.href="mailto:"+t+i+"@wavyindustries.com"}setTimeout(l,1e3);
---
+++ build_assets/hoisted.DXzGJ1yB.js
@@ -0,0 +1,1 @@
+import"./hoisted.DvC32OUy.js";function l(){let e=document.getElementById("something-special-kd3"),t="hell",i="o";e.href="mailto:"+t+i+"@wavyindustries.com"}setTimeout(l,1e3);
