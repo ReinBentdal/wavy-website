7f38f4fa8a800a31922e82c5dc7056bc38328953
ReinBentdal <rein.bent@gmail.com>
2025-05-02 00:42:17 +0200

    migrate to H

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   build: {
-    format: 'file',
+    format: 'directory',
     assets: 'build_assets'
   },
   vite: {
